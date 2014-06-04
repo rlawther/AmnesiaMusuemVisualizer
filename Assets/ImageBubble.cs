f22c774d9bef8e06b7eed1b21abe51e9dba929db
igsolo <icinema.lab@cse.unsw.edu.au>
2014-06-04 17:51:41 +1000

    only shows quads with priority > 0

--- Assets/ImageBubble.cs
+++ Assets/ImageBubble.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class ImageBubble : MonoBehaviour {
 
@@ -16,9 +17,16 @@ public class ImageBubble : MonoBehaviour {
 	void Update () {
 		var currentVis = this.visManager.visualizations [visManager.currentlySelectedVis];
 		foreach (var imageItem in currentVis.targetMetadataParser.output) {
+			try
+			{
 			var distance = Vector3.Distance(imageItem.transform.position,gameObject.transform.position);
 			var opacity = distance/divider -offset;
 			imageItem.material.color = new Color(1.0f, 1.0f, 1.0f,opacity);
+			} catch (NullReferenceException e)
+			{
+				/* FIXME : should handle parser item swithout quads (ie. priority *)
+				 * better than this. */
+			}
 		}
 	}
 }
